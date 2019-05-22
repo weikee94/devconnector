e69bfaba9d4527d3d184e21dfd0223eb9dc61a66
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-05-22 11:39:02 +0800

    custom auth middleware and jwt verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { check, validationResult } = require("express-validator/check");
+const config = require("config");
 
 const User = require("../../models/User");
 
@@ -65,7 +67,25 @@ router.post(
 
       await user.save();
 
-      res.send("User registered");
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ token });
+        }
+      );
+
+      //   res.send("User registered");
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
