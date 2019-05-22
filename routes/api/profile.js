78be577512df6556cf4d4334c2373e3fd598aff3
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-05-22 16:12:45 +0800

    get all profiles and profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -113,4 +113,42 @@ router.post(
   }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Server error");
+  }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user id
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate("user", ["name", "avatar"]);
+
+    if (!profile)
+      return res.status(400).json({
+        msg: "Profile not found"
+      });
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == "ObjectId") {
+      return res.status(400).json({
+        msg: "Profile not found"
+      });
+    }
+    return res.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
