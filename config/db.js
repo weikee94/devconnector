a8a77ee86d80c4f9bcd15cb9924be3489029e5ff
wei kee haw <weikeehaw@weis-MacBook-Pro.local>
2019-05-24 17:41:08 +0800

    test node config dir

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
+process.env.NODE_CONFIG_DIR = "./config";
+
 const config = require("config");
 const db = config.get("mongoURI");
 
 // this will return promise (async await using here)
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://admin123:admin123@devconnector-kkpu5.mongodb.net/test?retryWrites=true",
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false
-      }
-    );
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
     console.log("MongoDB Connected");
   } catch (err) {
     console.error(err.message);
